b2f6aac11c2f5f8a2f049dfa63314127b1f1db02
SohailPSK77 <sohail.psk77@gmail.com>
2026-09-21 05:28:04 +0530

    Refactor infix to postfix conversion logic

---
+++ prefix.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define MAX 100
+char stack[MAX];
+int top = -1;
+void push(char ch) {
+    stack[++top] = ch;
+}
+char pop() {
+    return stack[top--];
+}
+int precedence(char ch) {
+    if (ch == '^')
+        return 3;
+    else if (ch == '*' || ch == '/')
+        return 2;
+    else if (ch == '+' || ch == '-')
+        return 1;
+    else
+        return 0;
+}
+int isRightAssociative(char ch) {
+    return ch == '^';
+}
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i, j = 0;
+    char ch;
+    printf("Enter an infix expression: ");
+    scanf("%s", infix);
+    for (i = 0; infix[i] != '\0'; i++) {
+        ch = infix[i];
+        if (isalnum(ch)) {
+            postfix[j++] = ch;
+        }
+        else if (ch == '(') {
+            push(ch);
+        }
+        else if (ch == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[j++] = pop();
+            }
+            if (top != -1 && stack[top] == '(')
+                pop();
+        }
+        else {
+            while (top != -1 &&
+                   stack[top] != '(' &&
+                   (precedence(stack[top]) > precedence(ch) ||
+                   (precedence(stack[top]) == precedence(ch) &&
+                    !isRightAssociative(ch)))) {
+                postfix[j++] = pop();
+            }
+            push(ch);
+        }
+    }
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+    postfix[j] = '\0';
+    printf("Postfix expression: %s\n", postfix);
+    return 0;
+}
